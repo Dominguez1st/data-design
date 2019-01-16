fb796d5fbf5772dd6d3aa02766d1d88ef817e2ca
Robert Dominguez <dominguez.1st@gmail.com>
2019-01-16 16:54:35 -0700

    Mostly finished up conceptual-model.php. Looks like it it's almost done. Should just double-check in morning.

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -7,25 +7,39 @@
 	<body>
 		<h1>Conceptual Model</h1>
 		<h2>Entities & Attributes</h2>
-		<h3>Placeholder 1</h3>
+		<h3>User</h3>
 		<ul>
-			<li> </li>
-			<li> </li>
-			<li> </li>
+			<li>user</li>
+			<li>userId</li>
+			<li>userName</li>
+			<li>userPassword</li>
+			<li>userComment</li>
+			<li>userWatch</li>
 		</ul>
-		<h3>Placeholder 2</h3>
+		<h3>steamer</h3>
 		<ul>
-			<li> </li>
-			<li> </li>
-			<li> </li>
+			<li>steamerId</li>
+			<li>steamerName</li>
+			<li>steamerBroadcast</li>
 		</ul>
-		<h3>Placeholder 3</h3>
+		<h3>chat</h3>
 		<ul>
-			<li> </li>
-			<li> </li>
-			<li> </li>
+			<li>chatUserComment</li>>
+			<li>chatStreamerComment</li>
 		</ul>
-		<h2>Entity Relationship Diagram</h2>
+		<h3>stream</h3>
+		<ul>
+			<li>streamUserWatch</li>
+			<li>streamBroadcast</li>
+		</ul>
+		<h1>Relations</h1>
+		<ul>
+			<li>One streamer may only broadcast one stream</li>
+			<li>Many users may watch many streams</li>
+			<li>Many users may comment many times</li>
+			<li>Many streamers may comment many times</li>
+		</ul>
+		<h1>Entity Relationship Diagram</h1>
 		<a href="index.php">Return</a>
 	</body>
 </html>
