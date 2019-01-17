b597146833e3559e97f493b949c3b15766ea31a1
Robert Dominguez <dominguez.1st@gmail.com>
2019-01-17 16:00:13 -0700

    Did some spelling corrections. Think is't mostly done now apart from the MySQL

--- epic/index.php
+++ epic/index.php
@@ -7,9 +7,9 @@
 	<body>
 		<h1>Index</h1>
 		<ul>
-			<li><a href="michaelWilson.php">User Persona</a></li>
-			<li><a href="conceptual-model.php">Conceptual Model</a></li>
-			<li><a href="use-case.php">Use Case</a></li>
+			<h2><li><a href="michaelWilson.php">User Persona</a></li></h2>
+			<h2><li><a href="conceptual-model.php">Conceptual Model</a></li></h2>
+			<h2><li><a href="use-case.php">Use Case</a></li></h2>
 		</ul>
 	</body>
 </html>
--- epic/use-case.php
+++ epic/use-case.php
@@ -14,7 +14,7 @@
 		to listen/watch while he preforms menial tasks.</p>
 		<p><strong>Usage Preconditions: </strong>Must have had some prior knowledge of Twitch.tv and an idea of something
 			that he might be interested in listening to or watching</p>
-		<p><strong>Usage postconditions: </strong>Michael is being entertained and has the option to follow or subscribe to the streamer
+		<p><strong>Usage post conditions: </strong>Michael is being entertained and has the option to follow or subscribe to the streamer
 			that he is currently streaming.</p>
 		<p><strong>Interaction Flow: </strong></p>
 				<ul>
@@ -22,21 +22,17 @@
 					<li>The server returns Twitch's home page.</li>
 					<li>While on the home page, the home page will automatically begin streaming a featured stream as well as
 						listing other featured streams.</li>
-					<li>If Michael is not satisfied with the featured streams then he can scroll down to view streams that are
-						currently popular by viewer count and also separated by console. IE(Playstation 4 or Xbox One)</li>
-					<li>If Micheal is still not satisfied with his current selection then he can scroll to the top of the page
-					and click the text box with the icon of a magnifying glass.</li>
-					<li>Upon doing so Micheal will be able to type in the text either the name of a streamer that is currently
+					<li>Michael is not satisfied with the featured streams, he then scrolls down to view streams that are
+						currently popular by viewer count and also separated by console.</li>
+					<li>Micheal is still not satisfied with his current selection, he then scrolls to the top of the page
+					and clicks the text box with the icon of a magnifying glass.</li>
+					<li>Upon doing so Micheal is able to type in the text either the name of a streamer that is currently
 					online or the name of a game that he would be interested in watch.</li>
-					<li>When he has clicked on a streamer he will be watching and listening to the stream begin acquiring
-					entertainment</li>
-					<li>If Michael has instead chosen to click on a game then the Twitch will change the page and begin listing
-					all the streamers that are currently streaming that particular game.</li>
-					<li>At this point Michael can choose to click on a streamer and begin viewing the stream.</li>
-					<li>While viewing the stream Micheal can choose to click the button marked with a heart to follow the streamer
-					if Michael wants to receive notifications that the streamer is streaming again</li>
-					<li>If Micheal chooses to do this then he will be prompted to create a account, However this is a unnecessary
-					step as Michael does not have to create an account to view streamers.</li>
+					<li>Michael has chosen to type in the name of a game that he is interested in watching and hit search.</li>
+					<li>Twitch will change the page and begin listing all the streamers that are currently streaming that
+						particular game.</li>
+					<li>Michael passively watches and listens to the stream while doing other task</li>
+					<li>Micheal exits out satisfied</li>
 				</ul>
 		<p><strong>Frequency of Use: </strong>3 to 4 times per week.</p>
 		<h2><a href="index.php">Return</a></h2>
