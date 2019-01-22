b005bded01808af3147eaa06ffd072cc0c9a7a8f
Robert Dominguez <dominguez.1st@gmail.com>
2019-01-22 10:55:02 -0700

    Messed around with oml.sql

---
+++ sql/oml.sql
@@ -0,0 +1,4 @@
+ALTER DATABASE rdominguez45 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+INSERT INTO user (userId, userName, userPassword, userHosts) VALUES (UNHEX("c3bb03e3d581406d80ef00d3a69b59ee"),"nananana
+nananananananananananana","","")
